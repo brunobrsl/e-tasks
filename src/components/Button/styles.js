39d884e9ab2a199fa6f91251c78964469e8753ac
bruno <brunobalbrasil2@gmail.com>
2024-01-17 10:43:16 -0300

    adding responsiveness

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,14 +1,15 @@
 import styled from 'styled-components';
+import { DEVICE_BREAKPOINTS } from '../../styles/deviceBreakpoints';
 
 export const Container = styled.button`
   background-color: ${({ theme }) => theme.COLORS.BLUE_300};
   color: ${({ theme }) => theme.COLORS.BLUE_600};
   
   border: 0;
-  height: 56px;
-  padding: 0 16px;
+  height: 3.5rem;
+  padding: 0 1rem;
   font-weight: 500;
-  border-radius: 7px;
+  border-radius: 0.43rem;
   
   display: flex;
   align-items: center;
@@ -16,10 +17,23 @@ export const Container = styled.button`
   gap: 5px;
 
   > span {
-    font-size: 16px;
+    font-size: 1rem;
   }
 
   > svg {
-    font-size: 22px;
+    font-size: 1.37rem;
+  }
+
+  @media (max-width: ${DEVICE_BREAKPOINTS.XS}) {
+    height: 3rem;
+    padding: 0 0.5rem;
+
+    > span {
+      font-size: 0.8rem;
+    }
+
+    > svg {
+      font-size: 1rem;
+    }
   }
 `;
--- src/components/Filter/styles.js
+++ src/components/Filter/styles.js
@@ -2,13 +2,13 @@ import styled, { css } from 'styled-components';
 
 export const Container = styled.button`
   background: none;
-  font-size: 16px;
+  font-size: 1rem;
   border: 0;
 
   color: ${({ theme }) => theme.COLORS.GRAY_100};
 
   &:after {
-    display:block;
+    display: block;
     content: '';
     border-bottom: solid 3px ${({ theme }) => theme.COLORS.BLUE_300};  
     transition: all 250ms ease-in-out;
